786ebff2102e24a0428f0888ceabeb0907436d89
Gustavo Silva <gsilvaaragao@gmail.com>
2025-03-10 03:35:48 -0300

    Project Init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="px-6 py-2">
+      <p>
+        Hello World!
+      </p>
+    </main>
+  )
+}
---
+++ src/components/togglemode/component.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { useEffect, useState } from "react"
+import { FiSun, FiMoon } from "react-icons/fi"
+
+export default function ToggleMode() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  // Verifica o tema ao carregar a página
+  useEffect(() => {
+    const theme = localStorage.theme
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches
+
+    if (theme === "dark" || (!theme && systemPrefersDark)) {
+      document.documentElement.setAttribute("data-theme", "dark")
+      setIsDarkMode(true)
+    } else {
+      document.documentElement.setAttribute("data-theme", "light")
+      setIsDarkMode(false)
+    }
+  }, [])
+
+  // Troca tema
+  const toggleMode = () => {
+    if (isDarkMode) {
+      localStorage.theme = "light"
+      document.documentElement.setAttribute("data-theme", "light")
+    } else {
+      localStorage.theme = "dark"
+      document.documentElement.setAttribute("data-theme", "dark")
+    }
+    setIsDarkMode(!isDarkMode)
+  }
+
+  return (
+    <button
+      onClick={toggleMode}
+      className="fixed top-4 right-4 rounded-full p-2 bg-buttonsLight dark:bg-buttonsDark border-2 border-textLight dark:border-textDark">
+      {isDarkMode ? <FiMoon className="size-6"/> : <FiSun className="size-6"/>}
+    </button>
+  )
+}

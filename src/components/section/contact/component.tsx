5b11326875db4099a520e3c6481d73415a2457f5
Gustavo Silva <gsilvaaragao@gmail.com>
2025-03-16 00:01:23 -0300

    github contact add

--- src/components/section/contact/component.tsx
+++ src/components/section/contact/component.tsx
@@ -6,6 +6,7 @@ export default function Contact() {
       <LabelBox text="GUS.ARAGAO26@GMAIL" className="cursor-pointer" url="mailto:gus.aragao26@gmail.com"/>
       <LabelBox text="+55 (11) 98829-0222" className="cursor-pointer" url="https://wa.me/5511988290222?text=Vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20saber%20mais%20sobre%20seu%20trabalho!"/>
       <LabelBox text="LINKEDIN" className="cursor-pointer" url="https://www.linkedin.com/in/gustavo-aragao-dev/"/>
+      <LabelBox text="GITHUB" className="cursor-pointer" url="https://github.com/KaoreSactra/"/>
     </div>
   )
 }

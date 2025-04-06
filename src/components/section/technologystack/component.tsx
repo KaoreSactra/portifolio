67522d28a75dcea89221e581a8aab56eb3dde27e
Gustavo Silva <gsilvaaragao@gmail.com>
2025-04-05 23:51:07 -0300

    Stack Upgrade

--- src/components/section/technologystack/component.tsx
+++ src/components/section/technologystack/component.tsx
@@ -5,21 +5,22 @@ export default function TechnologyStack() {
   const t = useTranslations("Stack")
 
   const primaryStack = [
-    { id: 1, percentage: "100%", text: "TypeScript" },
-    { id: 2, percentage: "100%", text: "React" },
+    { id: 1, percentage: "100%", text: "Node.js" },
+    { id: 2, percentage: "100%", text: "Next.js" },
+    { id: 3, percentage: "100%", text: "TypeScript" },
   ]
   const secoundaryStack = [
-    { id: 1, percentage: "100%", text: "C#" },
-    { id: 2, percentage: "100%", text: ".NET" },
-    { id: 3, percentage: "100%", text: "Tailwind" },
-    { id: 4, percentage: "100%", text: "Next.js" },
-    { id: 5, percentage: "70%", text: "Electron" },
-    { id: 6, percentage: "50%", text: "Selenium" },
-    { id: 7, percentage: "100%", text: "JavaScript" },
-    { id: 8, percentage: "100%", text: "Git" },
-    { id: 9, percentage: "100%", text: "GitHub" },
-    { id: 10, percentage: "60%", text: "Python" },
-    { id: 11, percentage: "10%", text: "Flask" },
+    { id: 1, percentage: "100%", text: "React" },
+    { id: 2, percentage: "100%", text: "Tailwind" },
+    { id: 3, percentage: "100%", text: "JavaScript" },
+    { id: 4, percentage: "100%", text: "GitHub" },
+    { id: 5, percentage: "100%", text: "Git" },
+    { id: 9, percentage: "80%", text: "Python" },
+    { id: 10, percentage: "80%", text: "Flask" },
+    { id: 6, percentage: "70%", text: "Electron" },
+    { id: 7, percentage: "60%", text: ".NET" },
+    { id: 8, percentage: "60%", text: "C#" },
+    { id: 11, percentage: "50%", text: "Selenium" },
     { id: 12, percentage: "30%", text: "Java" },
   ]
 
